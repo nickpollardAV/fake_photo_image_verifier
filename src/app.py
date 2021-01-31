e2d3b8d75fa861649ea3a1c657f3cbfb8acbe392
nickpollardAV <nickpollard23@hotmail.com>
2021-01-31 11:05:38 +0000

    add test file

--- src/app.py
+++ src/app.py
@@ -1,17 +1,18 @@
 import json
-from controller import controller
+from controller import Controller
 
 
 
-def respond(res):
-    return {
-        'statusCode': 200,
-        'body': json.dumps(res),
-        'headers': {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin':'*'
-        }
-    }
+# def respond(res):
+#     return {
+#         'statusCode': 200,
+#         'body': json.dumps(res),
+#         'headers': {
+#             'Content-Type': 'application/json',
+#             'Access-Control-Allow-Origin':'*'
+#         }
+#     }
 
 def lambda_handler(event, context):
-    controller(event)
+    controller = Controller()
+    controller.run_steps(event)
---
+++ tests/unit/test_image_utils.py
@@ -0,0 +1,37 @@
+from unittest import mock, TestCase
+from unittest.mock import patch
+
+from PIL import Image
+from io import BytesIO
+
+from src.utils.image_utils import ImageUtils
+
+class UtilsTest(TestCase):
+
+    def setUp(self):
+        self.ImageUtils = ImageUtils()
+
+    def create_test_image(self):
+        # https://wildfish.com/blog/2014/02/27/generating-in-memory-image-for-tests-python/
+        file = BytesIO()
+        image = Image.new('RGBA', size=(2, 2), color=(155, 0, 0))
+        image.save(file, 'png')
+        file.name = 'test.png'
+        file.seek(0)
+        return file
+
+    def test_convert_image_to_numbers_list_opens_image_from_download_path(self):
+        with patch.object(Image, 'open') as mock:
+            self.ImageUtils.convert_image_to_numbers_list("test_path")
+        
+        mock.assert_called_with("test_path")
+
+    def test_convert_image_to_numbers_list_converts_image_to_rbg_array(self):
+        test_file_location = self.create_test_image()
+        result = self.ImageUtils.convert_image_to_numbers_list(test_file_location)
+
+        self.assertEqual(result, 155, 0, 0, 155, 0, 0, 155, 0, 0, 155, 0, 0)
+
+if __name__ == '__main__':
+    unittest.main()
+            
